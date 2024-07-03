a70eda93ae446e94a1fc69206835d03dcb55ea18
unknown <you@example.com>
2024-07-03 16:23:21 +0530

    customer_table SQL file

---
+++ customer_table.sql
@@ -0,0 +1,11 @@
+create table customers(
+	CUS_ID int,
+	NAME varchar(50),
+	CONTACT_NO varchar(10) 
+	primary key(CUS_ID)
+);
+
+insert into customers values (300,'Riya',7598462577);
+insert into customers values (301,'Diya',9847562845);
+insert into customers values (302,'Siya',8456966147);
+insert into customers values (303,'Piya',9647583211);
