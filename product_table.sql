eef51ded318a4a2a63fd372bd6078de57645dd1d
unknown <you@example.com>
2024-07-03 13:24:35 +0530

    Added product_table SQL file

---
+++ product_table.sql
@@ -0,0 +1,13 @@
+create table products(
+	PRO_ID int,
+	NAME varchar(50),
+	BRAND varchar(50),
+	PRICE int,
+	RATING float,
+	primary key(PRO_ID)
+);
+
+insert into products values(101,'Women shoes','Red tape',2500,4.1);
+insert into products values(102,'Women tops','Zara',700,3.9);
+insert into products values(103,'Men shirt','H & M',1500,4.0);
+insert into products values(104,'Handbags','Mango',2000,2.1);
